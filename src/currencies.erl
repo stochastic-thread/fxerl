d2814bac6ddc09ca31006557f9ff2ffd252ad277
ArthurColle <acolle@yandex.com>
2014-09-15 02:52:03 -0400

    Move application stop, for multiple starts

--- src/currencies.erl
+++ src/currencies.erl
@@ -1,12 +1,12 @@
 -module(currencies).
 -import(jsx, [decode/1]).
--export([start/0]).
+-export([start/0, stop/0]).
 
 start() -> 
 	URL = "http://openexchangerates.org/api/latest.json?app_id=2bcca613e8034f2cbd416a027258d599",
 	inets:start(),
 	{ok, Req} = httpc:request(URL),
-	{A, B, C} = Req,
+	{_, _, C} = Req,
 	D = list_to_binary(C),
 	E = jsx:decode(D),
 	Rates = proplists:get_value(<<"rates">>, E),
@@ -15,5 +15,6 @@ start() ->
 	JPY = proplists:get_value(<<"JPY">>, Rates),
 	io:format("1 dollar is ~p euro ~n",[EUR]),
 	io:format("1 dollar is ~p pound ~n",[GBP]),
-	io:format("1 dollar is ~p yen ~n",[JPY]),
-	init:stop().
+	io:format("1 dollar is ~p yen ~n",[JPY]).
+
+stop() -> init:stop().
